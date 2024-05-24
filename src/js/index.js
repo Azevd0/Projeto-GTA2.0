b097890824b4d87ef7ffecb909157e3d4608d6b5
Davyson <davyson887@gmail.com>
2024-05-24 11:23:52 -0300

    Recriando repositorio antigo

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const botao = document.querySelector(".btn-plataforma")
+const elementoplataformas = document.querySelector(".btn-plataforma .plataformas")
+
+botao.addEventListener("click", () => {
+    elementoplataformas.classList.toggle('ativo')
+})
